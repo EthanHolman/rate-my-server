67d63455d6e6d48c2ce878be189d68e7f2524e80
Ethan Holman <96ethanh@gmail.com>
2019-05-04 11:06:57 -0700

    Next step functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'rate-my-server';
   currentStep = 1;
+
+  nextStep() {
+    this.currentStep += 1;
+  }
 }
--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
+  @Output() next = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  nextStep() {
+    this.next.emit();
   }
 
 }
